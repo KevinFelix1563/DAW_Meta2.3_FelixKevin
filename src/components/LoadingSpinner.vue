667d0811b2c77df2e727ec7f2771dcf813a7fb96
Kevin Felix <kevin.felix59@uabc.edu.mx>
2026-03-08 21:15:08 -0700

    feat: LoadingSpinner.vue creado

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,3 +1,21 @@
 <template>
-  <div></div>
+  <div v-if="loading" class="d-flex justify-center align-center py-10">
+    <v-progress-circular
+      indeterminate
+      color="primary"
+      size="64"
+      width="6"
+    ></v-progress-circular>
+    <span class="ml-4 text-h6 text-grey">Buscando películas...</span>
+  </div>
 </template>
+
+<script setup>
+// Definimos que este componente recibe una propiedad (prop) llamada "loading" de tipo Booleano
+defineProps({
+  loading: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
